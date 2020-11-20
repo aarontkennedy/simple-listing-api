0427d850005e81821893167a351e6d9b3606f5c7
Kennedy <aarontkennedy@gmail.com>
2020-11-20 07:50:07 -0600

    finish crud

--- apiRoutes.js
+++ apiRoutes.js
@@ -9,86 +9,155 @@ function isValidDate(dateString) {
 
 module.exports = function (app) {
 
-    const orm = require("./models/orm.js");
+  const orm = require("./models/orm.js");
 
-    app.post('/create', (req, res) => {
-        const mlsNumber = req.query.mls_number;
-        if (!mlsNumber) {
-          res.status(400).json('provide a valid mls_number');
-        }
-        if (!req.query.address) {
-          res.status(400).json('provide a valid address');
-        }
-        if (!req.query.city) {
-          res.status(400).json('provide a valid city');
-        }
-        if (!req.query.state) {
-          res.status(400).json('provide a valid state');
-        }
-        if (!req.query.country) {
-          res.status(400).json('provide a valid country');
-        }
-        if (!req.query.zip) {
-          res.status(400).json('provide a valid zip code');
-        }
-        if (!req.query.list_date || !isValidDate(req.query.list_date)) {
-          res.status(400).json('provide a valid list_date YYYY-MM-DD');
-        }
-        if (!req.query.price || isNaN(req.query.price)) {
-          res.status(400).json('provide a valid price');
-        }
-        if (!req.query.agent_id) {
-          res.status(400).json('provide a valid agent_id');
-        }
+  app.post('/create', (req, res) => {
+    const mlsNumber = req.query.mls_number;
+    if (!mlsNumber) {
+      return res.status(400).json('provide a valid mls_number');
+    }
+    if (!req.query.address) {
+      return res.status(400).json('provide a valid address');
+    }
+    if (!req.query.city) {
+      return res.status(400).json('provide a valid city');
+    }
+    if (!req.query.state) {
+      return res.status(400).json('provide a valid state');
+    }
+    if (!req.query.country) {
+      return res.status(400).json('provide a valid country');
+    }
+    if (!req.query.zip) {
+      return res.status(400).json('provide a valid zip code');
+    }
+    if (!req.query.list_date || !isValidDate(req.query.list_date)) {
+      return res.status(400).json('provide a valid list_date YYYY-MM-DD');
+    }
+    if (!req.query.price || isNaN(req.query.price)) {
+      return res.status(400).json('provide a valid price');
+    }
+    if (!req.query.agent_id) {
+      return res.status(400).json('provide a valid agent_id');
+    }
+  
+    orm.get(mlsNumber)
+    .then((data) => { 
+      if (data.length > 0) {
+        return res.status(400).json(`${mlsNumber} already exists`);
+      }
       
-        orm.get(mlsNumber)
-        .then((data) => { 
-          if (data.length > 0) {
-            return res.status(400).json(`${mlsNumber} already exists`);
-          }
-          
-          orm.createListing(
-            mlsNumber,
-            req.query.address,
-            req.query.city,
-            req.query.state,
-            req.query.zip,
-            req.query.country,
-            req.query.list_date,
-            req.query.price,
-            req.query.agent_id
-          )
-          .then((data) => { return res.json('success'); })
-          .catch((error) => {
-              return res.json(error);
-          });
-      
-        })
-        .catch((error) => {
-            return res.json(error);
-        });
+      orm.createListing(
+        mlsNumber,
+        req.query.address,
+        req.query.city,
+        req.query.state,
+        req.query.zip,
+        req.query.country,
+        req.query.list_date,
+        req.query.price,
+        req.query.agent_id
+      )
+      .then((data) => { return res.json('success'); })
+      .catch((error) => {
+          return res.json(error);
+      });
+  
+    })
+    .catch((error) => {
+        return res.json(error);
     });
-      
-    app.get('/get', (req, res) => {
-        if (!req.query.mls_number) {
-            return res.status(400).send('provide a valid mls_number');
-        }
-        orm.get(req.query.mls_number)
-        .then((data) => { return res.json(data); })
-        .catch((error) => {
-            return res.json(error);
-        });
+  });
+    
+  app.get('/get', (req, res) => {
+    if (!req.query.mls_number) {
+        return res.status(400).send('provide a valid mls_number');
+    }
+    orm.get(req.query.mls_number)
+    .then((data) => { return res.json(data); })
+    .catch((error) => {
+        return res.json(error);
     });
+  });
 
-    app.get('/getbyagent', (req, res) => {
-        if (!req.query.agent_id) {
-            return res.status(400).send('provide a valid agent_id');
-        }
-        orm.getByAgent(req.query.agent_id)
-        .then((data) => { return res.json(data); })
-        .catch((error) => {
-            return res.json(error);
-        });
+  app.get('/getbyagent', (req, res) => {
+    if (!req.query.agent_id) {
+        return res.status(400).send('provide a valid agent_id');
+    }
+    orm.getByAgent(req.query.agent_id)
+    .then((data) => { return res.json(data); })
+    .catch((error) => {
+        return res.json(error);
     });
+  });
+
+  app.delete('/delete', (req, res) => {
+    if (!req.query.mls_number) {
+      return res.status(400).send('provide a valid mls_number');
+    }
+    orm.delete(req.query.mls_number)
+    .then((data) => { return res.json('success'); })
+    .catch((error) => {
+        return res.json(error);
+    });
+  });
+
+  app.put('/update', (req, res) => {
+    console.log('updating');
+    const mlsNumber = req.query.mls_number;
+    if (!mlsNumber) {
+      res.status(400).json('provide a valid mls_number');
+    }
+
+    let properties = {};
+
+    if (req.query.address) {
+      properties['address'] = req.query.address;
+    }
+    if (req.query.city) {
+      properties['city'] = req.query.city;
+    }
+    if (req.query.state) {
+      properties['state'] = req.query.state;
+    }
+    if (req.query.country) {
+      properties['country'] = req.query.country;
+    }
+    if (req.query.zip) {
+      properties['zip'] = req.query.zip;
+    }
+    if (req.query.list_date) {
+      if (!isValidDate(req.query.list_date)) {
+        return res.status(400).json('provide a valid list_date YYYY-MM-DD');
+      }
+      properties['list_date'] = req.query.list_date;
+    }
+    if (req.query.price) {
+      if (isNaN(req.query.price)) {
+        return res.status(400).json('provide a valid price');
+      }
+      properties['price'] = req.query.price;
+    }
+    if (req.query.agent_id) {
+      properties['agent_id'] = req.query.agent_id;
+    }
+  
+    orm.get(mlsNumber)
+    .then((data) => { 
+      if (data.length < 1) {
+        return res.status(400).json(`${mlsNumber} doesn't exist`);
+      }
+      
+      orm.update(mlsNumber, properties)
+      .then((data) => { return res.json('success'); })
+      .catch((error) => {
+          return res.json(error);
+      });
+  
+    })
+    .catch((error) => {
+        return res.json(error);
+    });
+  });
 
 };
--- models/orm.js
+++ models/orm.js
@@ -11,13 +11,14 @@ function performDatabaseCall(queryStr, parameters = null) {
             console.log('connected as id ' + connection.threadId);
 
             // conduct the actual requested query
-            connection.query(queryStr, parameters, function (err, result) {
+            var query = connection.query(queryStr, parameters, function (err, result) {
                 if (err) {
                     reject(err);
                 }
                 connection.release();
                 resolve(result);
             });
+            console.log(query.sql);
         });
     });
 }
@@ -58,69 +59,21 @@ const orm = {
         let queryString = `SELECT * FROM listings WHERE agent_id = ?;`;
         return performDatabaseCall(queryString, [agentId]);
     },
+    delete: function (mlsNumber) {
+        let queryString = `DELETE FROM listings WHERE mls_number = ?;`;
+        return performDatabaseCall(queryString, [mlsNumber]);
+    },
+    update: function (mlsNumber, properties) {
+        if (!properties || properties.length < 1) {
+            return 'nothing updated';
+        }
 
-//     getLearnersProblems: function (learnerID, problemCategory, limit=20) {
-//         var queryString = `
-// SELECT problems.*, problemStats.*, problemSubTypes.* FROM learners
-// INNER JOIN problemStats ON learners.google_id = problemStats.learner_id
-// INNER JOIN problems ON problemStats.problem_id= problems.problem
-// INNER JOIN problemSubTypes ON problemSubTypes.type= problems.type 
-// WHERE learners.google_id =? AND problems.category=?
-// ORDER BY problemStats.correct/problemStats.attempts,
-// problemSubTypes.difficulty, 
-// problemStats.streak, 
-// problems.ease LIMIT ?;`;
+        console.log(properties);
 
-//         return performDatabaseCall(queryString, [learnerID, problemCategory, limit]);
-//     },
+        let queryString = `UPDATE listings SET ? WHERE mls_number = ?;`;
 
-//     createLearnerFacts: function (learnerID, problemCategory) {
-//         let q = `
-//         INSERT INTO problemStats (learner_id, problem_id)
-//         SELECT ?, problem FROM problems WHERE problems.category=?;`;
-//         console.log(q);
-//         console.log(learnerID);
-//         console.log(problemCategory);
-
-//         return performDatabaseCall(q, [learnerID, problemCategory]);
-//     },
-
-//     updateLearnerProblemStats: function (problemStats) {
-//         let query = `
-// UPDATE problemStats
-// SET attempts=?, correct=?, streak=?, last_update=NOW()
-// WHERE problem_id=? AND learner_id=?;`;
-//         return performDatabaseCall(query,
-//             [problemStats.attempts,
-//             problemStats.correct,
-//             problemStats.streak,
-//             problemStats.problem_id,
-//             problemStats.learner_id]);
-//     },
-//     getLearnerStats: function (learnerID, problemCategory) {
-
-//         var queryString = `
-// SELECT problems.type,
-// AVG(problemStats.correct/problemStats.attempts) AS averageProficiencyPercent, 
-// SUM(IF(problemStats.attempts > 0, 1, 0))/COUNT(*) AS percentAttempted
-// FROM learners 
-// INNER JOIN problemStats ON learners.google_id = problemStats.learner_id 
-// INNER JOIN problems ON problemStats.problem_id= problems.problem 
-// INNER JOIN problemSubTypes ON problemSubTypes.type= problems.type
-// WHERE learners.google_id = ? AND problems.category = ? 
-// GROUP BY problems.type
-// ORDER BY problemSubTypes.difficulty;`;
-
-//         return performDatabaseCall(queryString, [learnerID,problemCategory]);
-//     },
-//     getRandomSupportImage: function () {
-
-//         var queryString = `SELECT inspirationalGiphies.url FROM inspirationalGiphies WHERE RAND()<=0.05 LIMIT 1;
-//         `;
-
-//         return performDatabaseCall(queryString, []);
-//     }
-
+        return performDatabaseCall(queryString, [properties, mlsNumber]);
+    },
 };
 
 module.exports = orm;
